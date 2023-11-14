2d848e64bc56183af953af16382dc6cd999cd5f1
ARSHAD19934 <arshadayyubbaghwan@gmail.com>
2023-11-14 15:56:46 +0530

    1st commit

---
+++ src/test/java/testyantra/CreateSampleTest.java
@@ -0,0 +1,14 @@
+package testyantra;
+
+import org.testng.annotations.Test;
+
+public class CreateSampleTest {
+	
+	@Test
+	
+	public void printTest()
+	{
+		System.out.println("arshadayyub");
+	}
+
+}
